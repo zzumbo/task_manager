62cd93e4b3b71663b8b9aa9798171ac99143770e
Zachary Zumbo <zachary.zumbo@gmail.com>
2025-01-21 11:07:26 -0700

    Add gitignore, remove node_modules

--- src/support/sidebar/Sidebar.tsx
+++ src/support/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyledNavbarLink } from './wrappers.ts';
+import React from "react";
+import { StyledNavbarLink } from "./wrappers.ts";
 
 /**
  * Sidebar
@@ -7,21 +7,21 @@ import { StyledNavbarLink } from './wrappers.ts';
 export const Sidebar: React.FC = () => (
   <div
     style={{
-      display: 'flex',
-      flexDirection: 'column',
-      backgroundColor: 'rgba(0,0,0,.3)',
-      padding: '1em',
-      gap: '1em',
+      display: "flex",
+      flexDirection: "column",
+      backgroundColor: "rgba(0,0,0,.3)",
+      padding: "1em",
+      gap: "1em",
     }}
   >
-    <h1>Transcend Task Management</h1>
-    <StyledNavbarLink href="/" current={window.location.pathname === '/'}>
+    <h1>Task Manager</h1>
+    <StyledNavbarLink href="/" current={window.location.pathname === "/"}>
       <i className="fa fa-list-check" />
       <span>Tasks</span>
     </StyledNavbarLink>
     <StyledNavbarLink
       href="/users"
-      current={window.location.pathname === '/users'}
+      current={window.location.pathname === "/users"}
     >
       <i className="fa fa-user" />
       <span>Users</span>
